51aede0b4446c025097e5a10a47a33df8a2b1183
Ehtisham33 <educatedshami@gmail.com>
2025-01-16 12:32:21 +0500

    delete

--- Model.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")  # Load a pre-trained model
-results = model.predict(source="dte_plycrd/Images/Images/2C0.jpg")  # Predict using an image
-print(results);
